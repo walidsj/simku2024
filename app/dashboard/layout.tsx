09bacdb7a6ec766f9c97614187c9efd742a7e4e4
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-01-04 02:01:56 +0800

    refactor: remove title in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,6 @@
-import { Metadata } from 'next'
 import Header from '@/components/partials/header'
 import Sidebar from '@/components/partials/sidebar'
 import { Flex } from '@mantine/core'
-
-export const metadata: Metadata = {
-    title: 'Dashboard',
-}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
